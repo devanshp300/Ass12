892051ab40664d082776636ad5fefb2feae8c941
Devansh291001 <devanshpatel2910@icloud.com>
2024-04-03 14:36:17 -0400

    Wrote and passed the tests

---
+++ src/main/java/com/example/assignmnet_lab12/AssignmnetLab12Application.java
@@ -0,0 +1,13 @@
+package com.example.assignmnet_lab12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmnetLab12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmnetLab12Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/assignmnet_lab12/AssignmnetLab12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignmnet_lab12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmnetLab12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
